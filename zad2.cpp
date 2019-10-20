a5bb52dcf553b3be0b219cc09b87595320071011
tursonsko <turekwojtek@gmail.com>
2019-10-20 23:09:46 +0200

    zad2 ladnie

--- zad2.cpp
+++ zad2.cpp
@@ -1,20 +1,36 @@
 #include <iostream>
-using namespace std;
-int main(){
-//Zuzia Ciborowska
-//Wojtek Turek
-//Krzysztof Mierzejewski
-int size;
-cout<<"Podaj rozmiar ";
-cin >>size;
-int i;
-int j;
-for (i=1;i<=size+1;i++){
-	for(j=size+1;j>i;j-- ){
-		cout<<" ";
-	}
-cout<<"*";
-cout<<endl;
 
+using namespace std;
+
+int main()
+{
+	int size, i, j;
+
+	cout << "Podaj rozmiar ";
+	cin >> size;
+	
+	for ( i = 1; i <= size + 1; i++ )
+	{
+		for( j = size + 1; j>i; j-- )
+		{
+			cout << " ";
+		}
+		
+		cout << "*";
+		cout << endl;
+	}
+
+	// for ( i = 1; i <= size; i++ )
+	// {
+	// 	for( j = size; j>i; j-- )
+	// 	{
+	// 		cout << " ";
+	// 	}
+		
+	// 	cout << "*";
+	// 	cout << endl;
+	// }
+
+
+return 0;
 }
-return 0;}
